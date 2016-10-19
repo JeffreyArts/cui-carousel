02800c0c7d3e4de45ad475311deb3aeb46acc5e6
Jeffrey Arts <jeffrey.arts@philips.com>
2016-10-19 22:07:27 +0200

    Update styling arrows

--- _js/helpers/find-ancestor.js
+++
@@ -1,8 +0,0 @@
-// function findAncestor (domElement, query) {
-//     var foundElement = domElement.parentElement.querySelector(query);
-//     if (foundElement) {
-//         return foundElement;
-//     }  else {
-//         return findAncestor(domElement.parentElement, query);
-//     }
-// }
